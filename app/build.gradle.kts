617bd96c629a1fd8f93e6a69b1593366b10f1cef
zkalmamatov <jolboldu39@gmail.com>
2024-10-25 12:38:18 +0600

    hm_02_06M

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,8 @@ dependencies {
     implementation ("androidx.navigation:navigation-ui-ktx:$navVersion")
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation (libs.okhttp)
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     kapt ("com.github.bumptech.glide:compiler:4.12.0")
--- app/src/main/java/com/example/hm01_06m/AppModule.kt
+++ app/src/main/java/com/example/hm01_06m/AppModule.kt
@@ -5,8 +5,11 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 
 @Module
@@ -14,12 +17,28 @@ import retrofit2.converter.gson.GsonConverterFactory
 object AppModule {
 
     @Provides
-    fun provideRetrofit(): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl(BuildConfig.BASE_URL).addConverterFactory(GsonConverterFactory.create())
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
 
-    }
+    @Provides
+    fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient.Builder()
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS)
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
     @Provides
     fun provideApi(retrofit: Retrofit): ApiService {
         return  retrofit.create(ApiService::class.java)
